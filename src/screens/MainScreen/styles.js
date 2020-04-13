e8d669792b56c120958576d5f2eec0257ff2efab
jpirazusta <52186485+jpirazusta@users.noreply.github.com>
2020-04-13 16:52:14 -0300

    Feature/map view (#8)
    * App rename
    
    * Add: Google map on main screen

--- src/screens/MainScreen/styles.js
+++ src/screens/MainScreen/styles.js
@@ -3,9 +3,15 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  map: {
+    flex: 5,
+  },
+  newTarget: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
