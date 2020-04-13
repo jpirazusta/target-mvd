65086358df0ab976b0146729febdf286f120967e
jpirazusta <52186485+jpirazusta@users.noreply.github.com>
2020-04-13 16:02:25 -0300

    Feature/signup (#7)
    * App rename
    
    * Feedback fixes

--- src/components/SignUpForm/index.js
+++ src/components/SignUpForm/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { func } from 'prop-types';
 import { View } from 'react-native';
 import { useStatus, LOADING } from '@rootstrap/redux-tools';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 import Dropdown from 'components/Dropdown';
 import ErrorView from 'components/common/ErrorView';
@@ -40,69 +41,71 @@ const SignUpForm = ({ onSubmit }) => {
 
   return (
     <View style={styles.container}>
-      <Input
-        label={strings.SIGN_UP.name}
-        testID="name-input"
-        error={errors[FIELDS.name]}
-        invalid={error || errors[FIELDS.name]}
-        {...inputProps(FIELDS.name)}
-        showErrorMessage
-      />
-      <Input
-        label={strings.SIGN_UP.email}
-        keyboardType="email-address"
-        autoCapitalize="none"
-        testID="email-input"
-        error={errors[FIELDS.email]}
-        invalid={error || errors[FIELDS.email]}
-        {...inputProps(FIELDS.email)}
-        showErrorMessage
-      />
-      <Input
-        label={strings.SIGN_UP.password}
-        placeholder={strings.SIGN_UP.passwordPlaceholder}
-        secureTextEntry
-        testID="password-input"
-        error={errors[FIELDS.password]}
-        invalid={error || errors[FIELDS.password]}
-        {...inputProps(FIELDS.password)}
-        showErrorMessage
-      />
-      <Input
-        label={strings.SIGN_UP.passwordConfirmation}
-        secureTextEntry
-        testID="confirm-password-input"
-        error={errors[FIELDS.passwordConfirmation]}
-        invalid={error || errors[FIELDS.passwordConfirmation]}
-        {...inputProps(FIELDS.passwordConfirmation)}
-        showErrorMessage
-      />
-      <View style={common.shortInputWidth}>
-        <Dropdown
-          label={strings.SIGN_UP.gender}
-          testID="gender-input"
-          items={[
-            { label: strings.SIGN_UP.female, value: 'female' },
-            { label: strings.SIGN_UP.male, value: 'male' },
-          ]}
-          error={errors[FIELDS.gender]}
-          invalid={error || errors[FIELDS.gender]}
-          placeholder={{
-            label: strings.SIGN_UP.selectGender,
-            value: null,
-          }}
-          {...inputProps(FIELDS.gender)}
+      <KeyboardAwareScrollView keyboardShouldPersistTaps="handled">
+        <Input
+          label={strings.SIGN_UP.name}
+          testID="name-input"
+          error={errors[FIELDS.name]}
+          invalid={error || errors[FIELDS.name]}
+          {...inputProps(FIELDS.name)}
           showErrorMessage
         />
-      </View>
-      <ErrorView errors={{ error }} />
-      <Button
-        testID="signup-submit-button"
-        handleOnPress={handleSubmit}
-        additionalStyles={styles.button}
-        title={strings.SIGN_UP.button}
-        isLoading={status === LOADING}
-      />
+        <Input
+          label={strings.SIGN_UP.email}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          testID="email-input"
+          error={errors[FIELDS.email]}
+          invalid={error || errors[FIELDS.email]}
+          {...inputProps(FIELDS.email)}
+          showErrorMessage
+        />
+        <Input
+          label={strings.SIGN_UP.password}
+          placeholder={strings.SIGN_UP.passwordPlaceholder}
+          secureTextEntry
+          testID="password-input"
+          error={errors[FIELDS.password]}
+          invalid={error || errors[FIELDS.password]}
+          {...inputProps(FIELDS.password)}
+          showErrorMessage
+        />
+        <Input
+          label={strings.SIGN_UP.passwordConfirmation}
+          secureTextEntry
+          testID="confirm-password-input"
+          error={errors[FIELDS.passwordConfirmation]}
+          invalid={error || errors[FIELDS.passwordConfirmation]}
+          {...inputProps(FIELDS.passwordConfirmation)}
+          showErrorMessage
+        />
+        <View style={common.shortInputWidth}>
+          <Dropdown
+            label={strings.SIGN_UP.gender}
+            testID="gender-input"
+            items={[
+              { label: strings.SIGN_UP.female, value: 'female' },
+              { label: strings.SIGN_UP.male, value: 'male' },
+            ]}
+            error={errors[FIELDS.gender]}
+            invalid={error || errors[FIELDS.gender]}
+            placeholder={{
+              label: strings.SIGN_UP.selectGender,
+              value: null,
+            }}
+            {...inputProps(FIELDS.gender)}
+            showErrorMessage
+          />
+        </View>
+        <ErrorView errors={{ error }} />
+        <Button
+          testID="signup-submit-button"
+          handleOnPress={handleSubmit}
+          additionalStyles={styles.button}
+          title={strings.SIGN_UP.button}
+          isLoading={status === LOADING}
+        />
+      </KeyboardAwareScrollView>
     </View>
   );
 };
