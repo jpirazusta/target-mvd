9deb89591856ce9c0186e489668a0c499a9924b9
Juan Pablo Irazusta <juanpabloirazusta@gmail.com>
2020-05-05 13:07:15 -0300

    Add: Conversations screen

--- src/components/ConversationItem/index.js
+++ src/components/ConversationItem/index.js
@@ -3,6 +3,7 @@ import { View, TouchableOpacity, Image, Text } from 'react-native';
 import { func } from 'prop-types';
 
 import Avatar from 'components/common/Avatar';
+<<<<<<< HEAD
 import Badge from 'components/common/Badge';
 import { CONVERSATION_SHAPE } from 'constants/shapes';
 import commonStyles from 'constants/commonStyles';
@@ -19,6 +20,18 @@ const ConversationItem = ({ conversation, onSelect }) => {
   return (
     <TouchableOpacity style={styles.row} onPress={onSelect}>
       <View style={commonStyles.row}>
+=======
+import { CONVERSATION_SHAPE } from 'constants/shapes';
+import common from 'constants/commonStyles';
+import styles from './styles';
+
+const ConversationItem = ({ conversation, onSelect }) => {
+  const { lastMessage, topicIcon, user } = conversation;
+  const { avatar, fullName } = user;
+  return (
+    <TouchableOpacity style={styles.row} onPress={onSelect}>
+      <View style={common.row}>
+>>>>>>> Add: Conversations screen
         <Avatar uri={avatar.smallThumbUrl} style={styles.avatar} />
         <View>
           <Text style={styles.name}>{fullName}</Text>
@@ -26,7 +39,10 @@ const ConversationItem = ({ conversation, onSelect }) => {
         </View>
       </View>
       <Image source={{ uri: topicIcon }} style={styles.topicIcon} />
+<<<<<<< HEAD
       {unreadMessages > 0 && <Badge number={unreadMessages} position={styles.badgePosition} />}
+=======
+>>>>>>> Add: Conversations screen
     </TouchableOpacity>
   );
 };
